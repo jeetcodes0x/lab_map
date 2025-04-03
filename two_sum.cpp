af7ec4528eda5da454f35bd889ec1502ca9684c8
Jeet Sakariya <jeet.sakariya06@gmail.com>
2025-04-03 14:20:38 +0530

    two_sum.cpp

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-       std::unordered_map<int, int> map; // Stores (number, index)
-    
+       std::unordered_map<int, int> map;
+        
     for (int i = 0; i < nums.size(); i++) {
         int complement = target - nums[i];
 
         if (map.find(complement) != map.end()) {
-            return {map[complement], i}; // Return stored index and current index
+            return {map[complement], i}; 
         }
-        map[nums[i]] = i; // Store current number and its index
+        map[nums[i]] = i; 
     }
     
     return {}; 
